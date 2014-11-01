64047986313e852a43d874177f32a397e828d563
jovchi <jovchi@yahoo.com>
2014-11-01 06:21:51 -0400

    new files and change PrimeGenerator to Class

--- PrimeGenerator.py
+++ PrimeGenerator.py
@@ -1,42 +1,49 @@
+""" Output prime numbers up to a given number
+"""
 
+class PrimeGenerator(object):
 
-limit=input("Please input a number: ")
-empty_string=""
+	prime_num=[]
 
-# checks if input is a number and not empty
-while not limit.isdigit() or limit==empty_string:
-	limit=input("Sorry, wrong input. Try again: ")
-
-
-x=int(limit)
-
-# checks if given number is prime
-def is_prime(num):
+	def __init__(self):
+		pass
 	
-	valid=False 
-	
-	if num<2:
+	# checks if given number is prime
+	def is_prime(self, num):
+		valid=False 
+		
+		if num<2:
+			return valid
+
+		i=num-1
+
+		while (num%i)!=0:
+			i=i-1
+		
+		if i==1:
+			valid=True
+
 		return valid
 
-	i=num-1
+	def run(self):
+		limit=input("Please input a number: ")
+		empty_string=""
 
-	while (num%i)!=0:
-		i=i-1
+		# checks if input is a number and not empty
+		while not limit.isdigit() or limit==empty_string:
+			limit=input("Sorry, wrong input. Try again: ")
+
+
+		x=int(limit)
+
 	
-	if i==1:
-		valid=True
-
-	return valid
-	
-prime_num=[]
+		for y in range(x):
+			if self.is_prime(y):
+			   self.prime_num.append(y)
 
 
-for y in range(x):
-	if is_prime(y):
-		prime_num.append(y)
+		print (" ".join(str(e) for e in self.prime_num))
 
-
-print (" ".join(str(e) for e in prime_num))
 
 
 
---
+++ Test_PrimeGenerator.py
@@ -0,0 +1,14 @@
+
+""" Test class for PrimeGenerator
+"""
+from PrimeGenerator import PrimeGenerator
+
+pg=PrimeGenerator()
+pg.run()
+
+
+
+
+
+
+

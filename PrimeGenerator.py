0d601376878b3b771a526eceae9a821626fc0595
blueitem <blueitem1@gmail.com>
2014-11-06 05:00:09 -0500

    Changed the way of taking input so it's not limited to strings, added optimization ideas sieve of Erathosthenes, only checks odd numbers and only mods the number against the square root + 1 instead of every number

--- PrimeGenerator.py
+++ PrimeGenerator.py
@@ -1,49 +1,50 @@
 """ Output prime numbers up to a given number
 """
+import math
 
 class PrimeGenerator(object):
+    prime_num = []
 
-	prime_num=[]
+    def __init__(self):
+        pass
 
-	def __init__(self):
-		pass
-	
-	# checks if given number is prime
-	def is_prime(self, num):
-		valid=False 
-		
-		if num<2:
-			return valid
+    # checks if given number is prime
+    def is_prime(self, num):
 
-		i=num-1
+        valid = False
 
-		while (num%i)!=0:
-			i=i-1
-		
-		if i==1:
-			valid=True
+        if num < 2:
+            return valid
 
-		return valid
+        i = int(math.sqrt(num) + 1)
 
-	def run(self):
-		limit=input("Please input a number: ")
-		empty_string=""
+        while (num % i) != 0:
+            i -= 1
 
-		# checks if input is a number and not empty
-		while not limit.isdigit() or limit==empty_string:
-			limit=input("Sorry, wrong input. Try again: ")
+        if i == 1:
+            valid = True
 
+        return valid
 
-		x=int(limit)
+    def run(self):
+        limit = input("Please input a number: ")
+        empty_string = ""
 
-	
-		for y in range(x):
-			if self.is_prime(y):
-			   self.prime_num.append(y)
+        # checks if input is a number and not empty
+        try:
+            x = int(limit)
+        except ValueError:
+            print "Sorry, your input was not a number"
+            return
 
+        if (x >= 2):
+            self.prime_num.append(2)
 
-		print (" ".join(str(e) for e in self.prime_num))
+        for y in range(1,x+1,2):
+            if self.is_prime(y):
+                self.prime_num.append(y)
 
+        print (" ".join(str(e) for e in self.prime_num))
 
 
 
